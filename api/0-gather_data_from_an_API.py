41b49b86eefed81c272e49479c5891c16cd796fc
chaher13 <chahercrbs@gmail.com>
2023-08-21 13:03:54 +0200

    up last luck

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,27 +16,27 @@
         user_id (int): The ID of the user for whom to retrieve completed tasks.
 
 """
-
-import requests
 import sys
+import requests
+
 
 
 def retrieve_data():
 
-    user_id = sys.argv[1]
+    user_id = int(sys.argv[1])
     user = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     todos = f'https://jsonplaceholder.typicode.com/todos/?userId={user_id}'
 
     response_user = requests.get(user)
     user_data = response_user.json()
-    name = user_data['name']
+    name = user_data.get("name")
     response_todos = requests.get(todos)
     todo_json = response_todos.json()
 
     complete = []
     for todo in todo_json:
-        if todo.get('completed') is True:
-            complete.append(todo['title'])
+        if todo["completed"] is True:
+            complete.append(todo.get("title"))
 
     print(f'Employee {name} is done\
 with tasks({len(complete)}/{len(todo_json)})')
